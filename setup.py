9908b0abab21985b0782a4fe4869f8ac8210503c
TyeYeah <wang98224@126.com>
2023-05-22 20:02:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='asm2vec',
     version='1.0.0',
     description='Unofficial implementation of asm2vec using pytorch',
-    install_requires=['torch>=1.7,<2'
-                      'click>=7.1,<8'
+    install_requires=['torch>=1.7,<2',
+                      'click>=7.1,<8',
                       'r2pipe>=1.5,<2'],
     author='oalieno',
     author_email='jeffrey6910@gmail.com',
