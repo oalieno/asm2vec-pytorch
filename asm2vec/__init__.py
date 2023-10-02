7e659f69de9b16a863e19621e2c76ea84b87e437
Jamie Nutter <64031696+jamienutter@users.noreply.github.com>
2023-10-02 13:40:32 +0100

    TRIVIAL - init

--- asm2vec/__init__.py
+++ asm2vec/__init__.py
@@ -1,6 +1,1 @@
-import importlib
-
-__all__ = ['model', 'datatype', 'utils']
-
-for module in __all__:
-    importlib.import_module(f'.{module}', 'asm2vec')
+__all__ = ["datatype", "model", "utils", "version"]
