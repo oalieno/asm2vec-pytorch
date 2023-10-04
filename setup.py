e8735feaa257b45996e3bb6d9871db6bbdae8fb5
Jamie Nutter <64031696+jamienutter@users.noreply.github.com>
2023-10-04 17:16:42 +0100

    AEGIS-6405 - radare2 test 2

--- setup.py
+++ setup.py
@@ -3,22 +3,29 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.install import install as _install
 
-from asm2vec.version import VERSION
+from asm2vec.version import VERSION, radare2_version
 
 
 class install(_install):
     @staticmethod
     def _setup_radare2() -> None:
         if sys.platform.startswith("linux"):
-            os.system("apt-get install radare2")
+            os.system("apt-get update")
+            os.system("apt-get install -y --no-install-recommends wget")
+            os.system(f"wget -O /tmp/radare2_${radare2_version}_arm64.deb https://github.com/radareorg/radare2/releases/download/${radare2_version}/radare2_${radare2_version}_arm64.deb")
+            os.system(f"dpkg -i /tmp/radare2_${radare2_version}_arm64.deb")
+            os.system("r2pm init")
+            os.system("r2pm update")
+            os.system(f"rm /tmp/radare2_${radare2_version}_arm64.deb")
         elif sys.platform.startswith("darwin"):
             os.system("brew install radare2")
         else:
             print("Ensure 'radar2' is installed...")
 
     def run(self):
-        _install.run(self)
         self._setup_radare2()
+        _install.run(self)
+
 
 
 def readme():
